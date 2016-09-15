89b95b12adb21f0f2f49e177477e4d57e55caa49
mattb230 <mr0tsplease>
2016-09-15 12:48:57 -0500

    Github Test

--- app/src/main/java/com/bootsysoftware/traininglog/HomeScreenActivity.java
+++ app/src/main/java/com/bootsysoftware/traininglog/HomeScreenActivity.java
@@ -2,6 +2,7 @@ package com.bootsysoftware.traininglog;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class HomeScreenActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class HomeScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        Toast.makeText(HomeScreenActivity.this, "Hello World!", Toast.LENGTH_SHORT).show();
     }
 }
